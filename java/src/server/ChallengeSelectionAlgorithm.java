eb923f6ec82a38a9decd1e2384f455ae941bd1b2
Jose Retana <62961706+joseff01@users.noreply.github.com>
2020-11-25 22:34:18 -0600

    Servidor y conexión inicial
    Se agregan los archivos y la clase main de java del servidor, junto con la clase donde se hará el algoritmo de challenges.
    Ahora se puede hacer la conexión inicial entre el cliente y el servidor, el cual se puede establecer ingresando el socket e ip y presionando enter

---
+++ java/src/server/ChallengeSelectionAlgorithm.java
@@ -0,0 +1,13 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+public class ChallengeSelectionAlgorithm {
+
+    public ChallengeSelectionAlgorithm(BufferedReader in, PrintWriter out){
+
+
+
+    }
+}
