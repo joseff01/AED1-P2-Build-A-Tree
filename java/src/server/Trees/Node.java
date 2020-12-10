62ee1d36a20a6842c784db10289643e0d5656a3b
Jose Retana <62961706+joseff01@users.noreply.github.com>
2020-12-10 15:12:45 -0600

    Cambios para serialización de árboles
    Se agregan getters, setters, y tags de jackson para que se serialicen correctamente los árboles

--- java/src/server/Trees/Node.java
+++ java/src/server/Trees/Node.java
@@ -1,13 +1,31 @@
 package server.Trees;
 public class Node {
+
     Node right,left;
-    int key,height;
+    int key;
+    int height;
 
     public Node(int Key){
         this.key=Key;
         this.left=this.right=null;
         this.height=0;
 
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
 }
